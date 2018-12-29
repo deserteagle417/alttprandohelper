85357aaa2eb2132c519edb7150ed6e614ec97fb6
Roger Norling <roger.norling.se@gmail.com>
2018-12-29 04:04:13 +0100

    Unit test all sphere 0 overworld locations

--- spec/model_spec.js
+++ spec/model_spec.js
@@ -18,6 +18,19 @@ describe('Model', () => {
 
         with_cases(
         ['lightworld_northwest', 'mushroom'],
+        ['lightworld_northwest', 'hideout'],
+        ['lightworld_northwest', 'well'],
+        ['lightworld_northwest', 'thief_hut'],
+        ['lightworld_northwest', 'bottle'],
+        ['lightworld_northwest', 'chicken'],
+        ['lightworld_northwest', 'tavern'],
+        ['lightworld_northeast', 'sahasrahla_hut'],
+        ['lightworld_south', 'maze'],
+        ['lightworld_south', 'link_house'],
+        ['lightworld_south', 'aginah'],
+        ['lightworld_south', 'dam'],
+        ['lightworld_south', 'lake_sw'],
+        ['lightworld_south', 'ice_cave'],
         (region, name) => it(`shows ${region} - ${name} as available`, () => {
             const actual = model.state();
             actual.lightworld[name].should.be.true;
--- src/model.js
+++ src/model.js
@@ -11,8 +11,27 @@
     const create_model = () => {
         return {
             state() {
-                const can_access = world.lightworld_northwest.locations.mushroom.can_access;
-                return { lightworld: { mushroom: !can_access } };
+                const { mushroom, hideout, well, thief_hut, bottle, chicken, tavern } =
+                    world.lightworld_northwest.locations;
+                const { sahasrahla_hut } = world.lightworld_northeast.locations;
+                const { maze, link_house, aginah, dam, lake_sw, ice_cave } =
+                    world.lightworld_south.locations;
+                return { lightworld: {
+                    mushroom: !mushroom.can_access,
+                    hideout: !hideout.can_access,
+                    well: !well.can_access,
+                    thief_hut: !thief_hut.can_access,
+                    bottle: !bottle.can_access,
+                    chicken: !chicken.can_access,
+                    tavern: !tavern.can_access,
+                    sahasrahla_hut: !sahasrahla_hut.can_access,
+                    maze: !maze.can_access,
+                    link_house: !link_house.can_access,
+                    aginah: !aginah.can_access,
+                    dam: !dam.can_access,
+                    lake_sw: !lake_sw.can_access,
+                    ice_cave: !ice_cave.can_access,
+                } };
             }
         }
     };
