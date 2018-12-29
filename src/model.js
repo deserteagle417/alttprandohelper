0d9316e91a8f9c4bff9939e90ac51ddd8aaffab8
Roger Norling <roger.norling.se@gmail.com>
2018-12-29 03:51:30 +0100

    Use world modul for model implementation

--- src/model.js
+++ src/model.js
@@ -1,14 +1,18 @@
 (function(root, factory) {
     if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
+        module.exports = factory(require('./world'));
     } else {
-        root.update = factory();
+        root.update = factory(root.create_world);
     }
-}(typeof self !== 'undefined' ? self : this, function() {
+}(typeof self !== 'undefined' ? self : this, function(create_world) {
+    const open_mode_setting = {};
+    const world = create_world(open_mode_setting).world;
+
     const create_model = () => {
         return {
             state() {
-                return { lightworld: { mushroom: true } };
+                const can_access = world.lightworld_northwest.locations.mushroom.can_access;
+                return { lightworld: { mushroom: !can_access } };
             }
         }
     };
